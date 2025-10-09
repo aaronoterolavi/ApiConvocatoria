d31df0d72e4cb31c1de8817f334cbcd3509d96c8
aaronoterolavi <aaronoterolavi@gmail.com>
2025-10-09 15:22:38 -0500

    .

--- Convocatorias.Application/DTOs/LoginResponseDto.cs
+++ Convocatorias.Application/DTOs/LoginResponseDto.cs
@@ -11,6 +11,10 @@ namespace Convocatorias.Application.DTOs
         public int IdUsuario { get; set; }
         public string CorreoElectronico { get; set; } = string.Empty;
         public string Nombres { get; set; } = string.Empty;
+        public string ApellidoPaterno { get; set; } = string.Empty;
+        public string ApellidoMaterno { get; set; } = string.Empty;
+        public int TipoDocumento { get; set; } 
+        public string NroDocumento {  get; set; } = string.Empty;
         public int CodRol { get; set; }
         public bool Activo { get; set; }
         public string Token { get; set; } = string.Empty;
